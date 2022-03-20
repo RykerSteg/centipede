911313d50be77e3c608b97c30f5d6840a85cf30b
RykerSteg <rykersteglich@gmail.com>
2022-03-19 23:43:15 -0600

    change to base files

--- centipede/game/services/keyboard_service.py
+++ centipede/game/services/keyboard_service.py
@@ -1,5 +1,3 @@
-# Kim
-
 import pyray
 from game.shared.point import Point
 
@@ -7,43 +5,40 @@ from game.shared.point import Point
 class KeyboardService:
     """Detects player input. 
     
-    The responsibility of a KeyboardService is to detect player key presses and translate them into 
-    a point representing a direction.
+    The responsibility of a KeyboardService is to indicate whether or not a key is up or down.
 
     Attributes:
-        cell_size (int): For scaling directional input to a grid.
+        _keys (Dict[string, int]): The letter to key mapping.
     """
 
-    def __init__(self, cell_size = 1):
-        """Constructs a new KeyboardService using the specified cell size.
+    def __init__(self):
+        """Constructs a new KeyboardService."""
+        self._keys = {}
+        
+        self._keys['w'] = pyray.KEY_W
+        self._keys['a'] = pyray.KEY_A
+        self._keys['s'] = pyray.KEY_S
+        self._keys['d'] = pyray.KEY_D
+
+        self._keys['i'] = pyray.KEY_I
+        self._keys['j'] = pyray.KEY_J
+        self._keys['k'] = pyray.KEY_K
+        self._keys['l'] = pyray.KEY_L
+
+    def is_key_up(self, key):
+        """Checks if the given key is currently up.
         
         Args:
-            cell_size (int): The size of a cell in the display grid.
+            key (string): The given key (w, a, s, d or i, j, k, l)
         """
-        self._cell_size = cell_size
+        pyray_key = self._keys[key.lower()]
+        return pyray.is_key_up(pyray_key)
 
-    def get_direction(self):
-        """Gets the selected direction based on the currently pressed keys.
-
-        Returns:
-            Point: The selected direction.
+    def is_key_down(self, key):
+        """Checks if the given key is currently down.
+        
+        Args:
+            key (string): The given key (w, a, s, d or i, j, k, l)
         """
-        dx = 0
-        dy = 0
-
-        if pyray.is_key_down(pyray.KEY_LEFT):
-            dx = -1
-        
-        if pyray.is_key_down(pyray.KEY_RIGHT):
-            dx = 1
-        
-        if pyray.is_key_down(pyray.KEY_UP):
-            dy = -1
-        
-        if pyray.is_key_down(pyray.KEY_DOWN):
-            dy = 1
-
-        direction = Point(dx, dy)
-        direction = direction.scale(self._cell_size)
-        
-        return direction
+        pyray_key = self._keys[key.lower()]
+        return pyray.is_key_down(pyray_key)
